e19b21f80098843da5c41f9b291e648d588c514a
Fenisu <44463720+Magesu@users.noreply.github.com>
2023-03-31 21:56:11 -0300

    Create ParMoeda.cs

---
+++ ParMoeda.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carteira_de_criptomoeda
+{
+    internal class ParMoeda
+    {
+        public Moeda moedaBase { get; set; }
+        public Moeda moedaCotacao { get; set; }
+        public double valor { get; set; }
+
+        public ParMoeda() { }
+
+        public ParMoeda(Moeda moedaBase, Moeda moedaCotacao, double valor)
+        {
+            this.moedaBase = moedaBase;
+            this.moedaCotacao = moedaCotacao;
+            this.valor = valor;
+        }
+
+        public void Imprime() // WIP
+        {
+            
+        }
+    }
+}
